792c6aa3c3162f81d91074cc493994e80e3c20f1
Kirill <pavlenkokirill120@gmail.com>
2024-08-05 17:59:10 +0300

    sqlalchemy models

---
+++ src/music/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Boolean, ForeignKey, LargeBinary, MetaData
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import (
+    Mapped,
+    DeclarativeBase,
+    mapped_column,
+    declared_attr,
+    relationship,
+)
+from music.enums import Genre, Role
+from config import settings
+
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+    
+
+class Song(Base):
+    name: Mapped[str]
+    genre: Mapped["Genre"]
+    artist_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    album_id: Mapped[int] = mapped_column(ForeignKey('album.id'), nullable=True)
+
+    artist: Mapped["User"] = relationship('Artist', back_populates='song')
+    album: Mapped["Album"] = relationship('Album', back_populates='songs')
+
+
+class Album(Base):
+    title: Mapped[str]
+    artist_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+ 
+    artist: Mapped["User"] = relationship('Artist', back_populates='albums')
+    songs: Mapped["Song"] = relationship('Song', back_populates='album')
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+    password_hash: Mapped[bytes] = mapped_column(LargeBinary)
+    active: Mapped[bool] = mapped_column(Boolean, default=True, server_default='true')
+    role: Mapped["Role"]
+
+    songs: Mapped[list["Song"]] = relationship('Song', back_populates='user')
+    albums: Mapped[list["Album"]] = relationship('Album', back_populates='user')
