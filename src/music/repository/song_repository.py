7422700ebfa9b49858afe94dd31b64c65d40fb30
Kirill <pavlenkokirill120@gmail.com>
2024-08-07 20:43:04 +0300

    Новая функция добавление песни в бд

--- src/music/repository/song_repository.py
+++ src/music/repository/song_repository.py
@@ -51,6 +51,22 @@ class SongRepository(AbstractRepository):
         songs: list[Song] = await session.scalars(stmt)
         return songs.all()
     
+    @staticmethod
+    async def create_song(
+        session: AsyncSession,
+        song_in: SongIn
+    ) -> None:
+        try:
+            song = Song(**song_in.model_dump())
+            session.add(song)
+            await session.commit()
+            return song
+        except Exception:
+            await session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Can not add song"
+            )
 
 # Зависимость для получения репозитория
 def get_song_repository() -> SongRepository:
