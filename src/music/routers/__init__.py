ef7a7cffe9ee9a8a03d4568536a9ade594266903
Kirill <pavlenkokirill120@gmail.com>
2024-08-13 12:38:35 +0300

    httpBEARER

--- src/music/routers/__init__.py
+++ src/music/routers/__init__.py
@@ -3,8 +3,10 @@ from fastapi.security import HTTPBearer
 from music.routers.song_router import router as song_router
 from music.routers.album_router import router as album_router
 
+# интерфейс для введения токена (который автоматически отправляеятся в заголовки) после логина
+http_bearer = HTTPBearer(auto_error=False)
 
-router = APIRouter()
+router = APIRouter(dependencies=[Depends(http_bearer)])
 
 router.include_router(song_router)
 router.include_router(album_router)
